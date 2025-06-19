51df708ddd2fcdf1e08253759071b306ec8dca2b
UpadyaDhananjani <upadyadhananjani3@gmail.com>
2025-06-19 15:14:12 +0530

    remove unwanted imports

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,13 +1,10 @@
-import express from 'express'
-import { register } from '../controllers/authController.js';
-import { login } from '../controllers/authController.js';
-import { logout } from '../controllers/authController.js';
-import e from 'express';
+import express from 'express';
+import { register, login, logout } from '../controllers/authController.js';
 
 const authRouter = express.Router();
 
-authRouter.post('/register',register);
-authRouter.post('/login',login);
-authRouter.post('/logout',logout);
+authRouter.post('/register', register);
+authRouter.post('/login', login);
+authRouter.post('/logout', logout);
 
 export default authRouter;
