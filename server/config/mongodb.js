d0913ecd81a2cb81c9ca08d0e0162c3150b4dcdb
Pooji0311 <IT22254184@my.sliit.lk>
2025-06-19 11:11:59 +0530

    mongodb connection

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
-
-    mongoose.connection.on('connected', ()=>console.log("Database connected"));
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { register } from '../controllers/authController.js';
+import e from 'express';
 
 const authRouter = express.Router();
 
 authRouter.post('/register',register);
 authRouter.post('/login',register);
 authRouter.post('/logout',register);
+
+export default authRouter;
