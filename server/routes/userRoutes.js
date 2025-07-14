68d3cae2f9a4ace6f4094e2b0a7502bba93344af
Pooji0311 <IT22254184@my.sliit.lk>
2025-07-14 16:45:31 +0530

    update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import userAuth from '../middleware/userAuth.js';
 import { getUserData, getUsersByUnit } from '../controllers/userController.js';
-import { getUserData, getUsersByUnit } from '../controllers/userController.js';
+
 
 const userRouter = express.Router();
 
 userRouter.get('/data', userAuth, getUserData);
 userRouter.get('/by-unit/:unit', userAuth, getUsersByUnit);
-userRouter.get('/by-unit/:unit', userAuth, getUsersByUnit);
+
 
 export default userRouter;
